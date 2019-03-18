fe2920dc6d2dbdcba754a6535abbade05d20efc7
Marc <marc.purgimon@gmail.com>
2019-03-18 17:09:03 +0100

    Data amb filtres

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+ var post = [];
+ var horaIncial = 0;
+ var horaFinal = 0;
+ var diaInicial = 0;
+ var diaFinal = 0;
+
+
+
 $(function () {
 
-    var post = [];
-    var horaIncial = 0;
-    var horaFinal = 0;
-    var diaInicial = 0;
-    var diaFinal = 0;
+
 
     function getMonth(m) {
         var n = 0;
@@ -13,57 +17,74 @@ $(function () {
                 n = 1;
                 break;
             case "February":
-                break;
                 n = 2;
+                break;
+    
             case "March":
-                break;
                 n = 3;
+                break;
+    
             case "April":
+                n = 4;
                 break;
-                n = 44;
+    
             case "May":
-                break;
                 n = 5;
+                break;
+    
             case "June":
-                break;
                 n = 6;
+                break;
+    
             case "July":
-                break;
                 n = 7;
+                break;
+    
             case "August":
-                break;
                 n = 8;
+                break;
+    
             case "September":
-                break;
                 n = 9;
+                break;
+    
             case "Octover":
-                break;
                 n = 10;
-            case "November":
                 break;
+    
+            case "November":
                 n = 11;
+                break;
+    
             case "December":
                 n = 12;
+    
                 break;
         }
-
+    
         return n;
-
+    
     }
-
-    function getDates() {
-
+    
+    
+    function obtenirDates() {
+        
         horaIncial = post[0] + ":" + post[1];
         diaInicial = post[4] + "-" + getMonth(post[3]) + "-" + post[2];
+        diaInicial += " "+ horaIncial;
         horaFinal = post[5] + ":" + post[6];
         diaFinal = post[9] + "-" + getMonth(post[8]) + "-" + post[7];
-
+        diaFinal += " "+ horaFinal;
+    
     }
 
 
-    function dibuixaGrafica(ctx, fila, nom){
+
+
+
+    function dibuixaGrafica(ctx, creat, fila, nom) {
         var chartdata = {
-            labels: fila,
+            labels: creat,
             datasets: [{
                 label: nom,
                 backgroundColor: 'rgba(200, 200, 200, 0.75)',
@@ -74,7 +95,7 @@ $(function () {
             }]
         };
 
-        
+
 
         var barGraph = new Chart(ctx, {
             type: 'line',
@@ -85,76 +106,87 @@ $(function () {
 
 
 
-$("#HoraIni").change(function () {
-    $("select option:selected").each(function () {
-        post.push($(this).text());
-        console.log($(this).text());
+    $("select").change(function () {
+        post = [];
+        $("select option:selected").each(function () {
+            post.push($(this).text());
+            console.log($(this).text());
+        });
+        console.log(post);
+
+
     });
-    console.log(post);
 
-    getDates();
-});
+    $("#formulari").submit(function (event) {
 
-$("#formulari").submit(function (event) {
-    console.log("Submit");
 
-    event.preventDefault();
-    $.ajax({
-        url: "http://localhost/calendariPHP/somewhere.php",
-        method: "GET",
-        success: function (data) {
-            console.log("Funciona");
-            console.log(data);
 
-            var obj = $.parseJSON(data);
-            var created_atX = [];
-            var ModuleCurrentX = [];
-            var Battery = [];
-            var Lig = [];
+        console.log("Submit");
 
-            //x="2018-12-17 19:22:18";
-            for (var i in obj) {
-                // if(obj[i].created_at>=data1 && obj[i].created_at<=data2){
-                created_atX.push(obj[i].created_at);
-                ModuleCurrentX.push(obj[i].ModuleCurrent);
-                Battery.push(obj[i].BatterySoC);
-                Lig.push(obj[i].LightingPower);
-                //}
-                //else console.log("rip");
+        event.preventDefault();
+        $.ajax({
+            url: "http://localhost/calendariPHP/somewhere.php",
+            method: "GET",
+            success: function (data) {
+                console.log("Funciona");
+                console.log(data);
 
-            }
+                obtenirDates();
 
-            /* var chartdata = {
-                labels: created_atX,
-                datasets: [{
-                    label: 'ModuleCurrent',
-                    backgroundColor: 'rgba(200, 200, 200, 0.75)',
-                    borderColor: 'rgba(200, 200, 200, 0.75)',
-                    hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-                    hoverBorderColor: 'rgba(200, 200, 200, 1)',
-                    data: ModuleCurrentX
-                }]
-            }; */
+               
+                console.log(diaInicial);
+               
+                console.log(diaFinal);
 
-            var ctx = $("#myCanvas");
-            dibuixaGrafica(ctx,ModuleCurrentX, "ModuleCurrentX" );
+                var obj = $.parseJSON(data);
+                var created_atX = [];
+                var ModuleCurrentX = [];
+                var Battery = [];
+                var Lig = [];
 
-             ctx = $("#myCanvas1");
+                //x="2018-12-17 19:22:18";
+                for (var i in obj) {
+                     if(obj[i].created_at>=diaInicial && obj[i].created_at<=diaFinal){
+                    created_atX.push(obj[i].created_at);
+                    ModuleCurrentX.push(obj[i].ModuleCurrent);
+                    Battery.push(obj[i].BatterySoC);
+                    Lig.push(obj[i].LightingPower);
+                    }
+                    else console.log("rip");
 
-             dibuixaGrafica(ctx, Battery, "Battery");
-            ctx = $("#myCanvas2");
+                }
 
-            dibuixaGrafica(ctx, Lig, "Lig");
+                /* var chartdata = {
+                    labels: created_atX,
+                    datasets: [{
+                        label: 'ModuleCurrent',
+                        backgroundColor: 'rgba(200, 200, 200, 0.75)',
+                        borderColor: 'rgba(200, 200, 200, 0.75)',
+                        hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+                        hoverBorderColor: 'rgba(200, 200, 200, 1)',
+                        data: ModuleCurrentX
+                    }]
+                }; */
 
-            /* var barGraph = new Chart(ctx, {
-                type: 'line',
-                data: chartdata
-            }); */
-        },
+                var ctx = $("#myCanvas");
+                dibuixaGrafica(ctx, created_atX,  ModuleCurrentX, "ModuleCurrentX");
 
-        error: function () {
-            console.log("No funciona");
-        },
+                ctx = $("#myCanvas1");
+
+                dibuixaGrafica(ctx, created_atX, Battery, "Battery");
+                ctx = $("#myCanvas2");
+
+                dibuixaGrafica(ctx, created_atX, Lig, "Lig");
+
+                /* var barGraph = new Chart(ctx, {
+                    type: 'line',
+                    data: chartdata
+                }); */
+            },
+
+            error: function () {
+                console.log("No funciona");
+            },
+        });
     });
-});
 });
